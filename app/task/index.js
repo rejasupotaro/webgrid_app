7b0740b62e30d6db7dc95f92fdb692d22b5860f9
Kentarou Takiguchi <takiguchi0817@gmail.com>
2012-07-08 11:44:14 +0900

    タスクファイルへのコールバック的なあれ
    - EventEmitterを使ってタスクが終了したらmain.result()を呼び出すように

---
+++ app/task/index.js
@@ -0,0 +1,48 @@
+var TaskManager = require("../../node_modules/webgrid/lib/taskManager")
+var taskManager = new TaskManager()
+
+module.exports.main = function() {
+  var primaryTest = function() {
+    var args = arguments[0]
+    var result = new Array()
+
+    for (var i = 0; i < args.length; i++) {
+      var num = args[i]
+      var flag = true
+      var limit = Math.sqrt(num)
+      for (var j = 2; j <= limit; j++) {
+        if (num % j == 0) {
+          flag = false
+          break
+        }
+      }
+      if (flag) {
+        result.push(num)
+      }
+    }
+    return result
+  }
+
+  //var dataNum = 10000000
+  //var range =   100
+  var dataNum = 100
+  var range = 10
+  for (var i = 0; i < dataNum / range; i++) {
+    var args = new Array();
+    for (var j = 0; j < range; j++) {
+      var n = i * range + j
+      if (n != 0 && n != 1 && n != 2) {
+        args.push(n);
+      }
+    }
+    taskManager.addTask(primaryTest, args)
+  }
+
+  return this
+}
+
+module.exports.result = function(result) {
+  console.log('\n---------------------')
+  console.log('100までの素数のリスト')
+  console.log(result.toString())
+}
